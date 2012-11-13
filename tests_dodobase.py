fbc7b6d66e070ca20e3ea0057e088c4dae7459b2
Ethan White <ethan@weecology.org>
2012-11-13 09:26:29 -0700

    Splitting tests into two files in preparation for shift to using nose

--- tests_dodobase.py
+++ tests_dodobase.py
@@ -1,7 +1,5 @@
 from unittest import TestCase, main
 from dodobase.tools.tax_resolve import get_synonyms, tax_resolve_fuzzy
-#from dodobase.tools.get_mendeley_data import get_mendeley_data, citation
-
 
 class TestTaxResolve(TestCase):
     def setUp(self):
@@ -37,35 +35,6 @@ class TestTaxResolve(TestCase):
     def test_mosquitos_case_sensitivty(self):
         for to_test in ['Aedes clivis', 'Aedes Clivid', 'ochlerotatus clivis', 'Ochlerotatus Clivid']:
             self.assertEqual(tax_resolve_fuzzy(to_test, synonyms=self.syn2), 'Aedes clivis')
-            
-#class TestMendeleyTags(TestCase):
-    #def setUp(self):
-        #self.urls = [
-                     #"http://www.mendeley.com/research/niche-neutrality/",
-                     #"http://www.mendeley.com/research/local-interactions-select-lower-pathogen-infectivity/",
-                     #"http://www.mendeley.com/research/widespread-amphibian-extinctions-epidemic-disease-driven-global-warming/",
-                     #]
-
-        #self.data_docs = []
-        #self.citations = []
-        #for url in self.urls:
-            #self.data_docs.append(get_mendeley_data(url))
-            #self.citations.append(citation(url))
-
-        #print '\n\n'.join(self.citations)
-
-    #def test_mendeley_tags(self):
-        #for data_doc, citation, (title, year, published_in, in_citation) in zip(self.data_docs, self.citations,
-        #[
-         #('A niche for neutrality', 2007, 'Ecology Letters', 'Adler, P. B.'),
-         #('Local interactions select for lower pathogen infectivity', 2007, 'Science', 'Boots, M.'),
-         #('Widespread amphibian extinctions from epidemic disease driven by global warming', 2006, 'Nature', 'Pounds, J. A.'),
-        #]):
-            #self.assertEqual(data_doc['title'], title)
-            #self.assertEqual(data_doc['year'], year)
-            #self.assertEqual(data_doc['published_in'], published_in)
-            #self.assertIn(in_citation, citation)
-
 
 if __name__ == '__main__':
     main()
